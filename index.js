eb34a8b5c8bd528af4daec18e1a4af1d1c3713e0
Simranjit Kaur Maan <simranjitkaur94mec@gmail.com>
2021-07-17 04:15:32 +0000

    Added conditionals

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 var readlineSync = require('readline-sync');
-var userAge = readlineSync.question('am i older than 25? ')
+var questionOne = "am i older than 25? ";
+var answerOne = "yes";
+var answerTwo = "no";
+var userAnswer = readlineSync.question(questionOne);
+
+var score = 0;
+
+if(userAnswer === "yes"){
+ console.log(answerOne);
+ score+=1;
+}
+else{console.log(answerTwo);
+score-=1;
+}
+
+console.log(score);
 
 
-if(userAge === "yes")
- console.log('You are right!');
-else console.log('You are wrong!');
+questionTwo = "are you from USA?";
+answerOne = "yes";
+answerTwo = "no";
+userAnswer = readlineSync.question(questionTwo);
 
+if(userAnswer === "yes"){
+ console.log(answerOne);
+ score+=1;
+}
+else{console.log(answerTwo);
+score-=1;
+}
 
+console.log(score);
