781e872fcf1db1e8f7dceb0c1c1545b86c35db23
Simranjit Kaur Maan <simranjitkaur94mec@gmail.com>
2021-07-17 03:48:08 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+//console.log("Simran")
+//prompt("Give me your name")
+
+var readlineSync = require('readline-sync');
+var userName = readlineSync.question('May I have your name? ');
+console.log("Welcome "+ userName)
+
