0d7288184ff6bac0ed7b8e1675e558adee99408a
Simranjit Kaur Maan <simranjitkaur94mec@gmail.com>
2021-07-17 04:49:12 +0000

    functions calls

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ function add(numberOne, numberTwo) {
 
 var result = add(1,2);
 console.log("Sum of 1 and 2: "+ result);
+
+
+function product(numberOne, numberTwo) {
+  var product = numberOne * numberTwo;
+  return product;
+}
+
+result = product(1,2);
+console.log("Product of 1 and 2: "+ product(1,2));
+
