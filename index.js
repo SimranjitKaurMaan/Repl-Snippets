4b571a5783e9f530321da8371b8660e36a8b5017
Simranjit Kaur Maan <simranjitkaur94mec@gmail.com>
2021-07-17 04:04:34 +0000

    If else

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 var readlineSync = require('readline-sync');
+var userAge = readlineSync.question('am i older than 25? ')
+
+
+if(userAge === "yes")
+ console.log('You are right!');
+else console.log('You are wrong!');
 
 
