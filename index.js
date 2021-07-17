47970484418da6d10444e810c13295a4a28b9851
Simranjit Kaur Maan <simranjitkaur94mec@gmail.com>
2021-07-17 07:46:25 +0000

    Object basics

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-var groceryList = ["milk","egg","pav","samosa","flowers"]
-console.log(groceryList[0])
-console.log(groceryList.length)
+var superman = {
+  name: "superman",
+  power: "flight",
+  strength: 10000,
+  stealth: 0
+}
 
-for(var i=0;i<groceryList.length;i++){
-  console.log(groceryList[i])
+var batman = {
+  name: "batman",
+  power: "stealth",
+  strength: 100,
+  stealth: 1000
+}
+
+//console.log(superman.power)
+
+//console.log(batman.power)
+
+var superheros = [superman,batman]
+
+for(var i=0;i<superheros.length;i++){
+  console.log(superheros[i].name + ": "+ superheros[i].stealth)
 }
