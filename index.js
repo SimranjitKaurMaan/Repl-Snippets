18dbcff60823abe38ba12be0ed9a506ddde29613
Simranjit Kaur Maan <simranjitkaur94mec@gmail.com>
2021-07-17 03:59:38 +0000

    Removed console messages

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-//console.log("Simran")
-//prompt("Give me your name")
-
 var readlineSync = require('readline-sync');
-var userName = readlineSync.question('May I have your name? ');
-console.log("Welcome "+ userName)
+
 
