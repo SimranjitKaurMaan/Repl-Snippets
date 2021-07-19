b212d642e77df433590753a331c2ebb8112c768b
Simranjit Kaur Maan <simranjitkaur94mec@gmail.com>
2021-07-19 15:18:43 +0000

    updated endgame

--- index.js
+++ index.js
@@ -1,8 +1,63 @@
-//console.log("Simran")
-//prompt("Give me your name")
-
 var readlineSync = require('readline-sync');
-var userName = readlineSync.question('May I have your name? ');
+var userName = readlineSync.question('What is your name? ');
+
+var score=0;
 console.log("Welcome "+ userName)
 
-console.log("hello")
+<<<<<<< end-game
+console.log('Let\'s check if you can manage your finances independently?')
+
+// play function 
+function play(question,answer){
+  var userAnswer = readlineSync.question(question);
+  if(userAnswer === answer){
+    score++;
+  }else{
+    score--;
+  }
+
+  console.log("Current score: "+ score);
+}
+
+var questions = [{
+  question : "You know what\'s index funds? ",
+  answer: "Yes"
+},{
+  question : "You know what\'s ETFs? ",
+  answer: "Yes"
+},{
+  question : "You know why low fees matter? ",
+  answer: "Yes"
+},{
+  question : "You invest in index funds and ETFs? ",
+  answer: "Yes"
+},{
+  question : "You know about diversification? ",
+  answer: "Yes"
+},{
+  question : "You save taxes with NPS? ",
+  answer: "Yes"
+},{
+  question : "You are putting away atleast 20% of your paycheck into savings? ",
+  answer: "Yes"
+},{
+  question : "If market plummets you don't sell everything and you know you are in it for the long term? ",
+  answer: "Yes"
+},{
+  question : "You have setup the set it forget it model? ",
+  answer: "Yes"
+},{
+  question : "You have health insurance and life insurance? ",
+  answer: "Yes"
+}];
+
+for(var i=0;i<questions.length;i++){
+ play(questions[i].question,questions[i].answer);
+}
+
+console.log("Your score is : "+ score)
+if(score>6){
+  console.log("Congrats! You know how to manage your fincances independently.You don't likely need to pay for financial advice.")
+}else{
+  console.log("Don't worry.You can read up on a few books like Unconventional Success by David F. Swensen and Common Sense on Mutual Funds by John C. Bogle to get going.")
+}
